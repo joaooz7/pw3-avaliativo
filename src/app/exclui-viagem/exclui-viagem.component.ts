493c84ea15dc82a2c7df2a58fa6ca0b96e5b2a99
Your Name <you@example.com>
2023-06-21 09:31:21 -0300

    site pronto

--- src/app/exclui-viagem/exclui-viagem.component.ts
+++ src/app/exclui-viagem/exclui-viagem.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./exclui-viagem.component.css']
 })
 export class ExcluiViagemComponent {
-
+  viagens = [
+    {"destino":"Acapulco", "tipo":"LAZER", "data_chegada":"08/06/2021", "data_saida":"18/06/2021"},
+ ];
 }
